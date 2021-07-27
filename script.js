cbb01cccf19df2c17aeb18eebdc1bfd7cd5df257
Sammienator <sammugo18@gmail.com>
2021-07-27 17:03:25 +0300

    changes

---
+++ script.js
@@ -0,0 +1,10 @@
+// const ap = new APlayer({
+//     container: document.getElementById('aplayer'),
+//     listFolded: true,
+//     audio: [{
+//         name: 'Invisible Beauty',
+//         artist: 'Uncle Sam',
+//         url: 'invisible_beauty.mp3',
+//         cover: 'image/1.jpeg'
+//     }]
+// });
